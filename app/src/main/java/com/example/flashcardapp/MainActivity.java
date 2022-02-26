93f91e6c2d9773b5106a50f3509e9b2b66c1a859
Dennis <13dennissantoso@gmail.com>
2022-02-26 11:35:57 -0800

    Initial commit

---
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.flashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView flashcardQuestion = findViewById(R.id.flashcard_question);
+        TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+        flashcardQuestion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
+            }
+        });
+        flashcardAnswer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                flashcardAnswer.setVisibility(View.INVISIBLE);
+                flashcardQuestion.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
